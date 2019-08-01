aefab322136cca7b53999ef615e86d244039a20d
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-08-01 13:39:47 -0500

    creando modulo de la intrefaz

---
+++ js/interfaz.js
@@ -0,0 +1,4 @@
+export const formularioBuscar = document.querySelector('#formulario-buscar'),
+             divBuscar = document.querySelector('#buscar'),
+             divMensajes = document.querySelector('#mensajes'),
+             divResultados = document.querySelector('#resultado');
