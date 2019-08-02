210eacd40cd1a3a642e05605bc3dcc16aa711db9
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-08-02 11:57:04 -0500

    mostrando las consulta en ui

--- js/app.js
+++ js/app.js
@@ -31,8 +31,27 @@ UI.formularioBuscar.addEventListener('submit', (e) => {
            .then(data => {
 
                 console.log(data);
+
+                if(data.respuesta.lyrics){
+
+                    const letra = data.respuesta.lyrics;
+
+                    UI.divResultados.textContent = letra;
+
+                }else{
+                    
+                    UI.divMensajes.innerHTML = 'La cancion no existe, prueba con otra';
+                    UI.divMensajes.classList.add('error');
+                    
+                    setTimeout(() => {
+                        
+                        UI.divMensajes.innerHTML = '';
+                        UI.divMensajes.classList.remove('error');
+                        UI.formularioBuscar.reset();
+            
+                    }, 3000);
+                }
            });
     }
 
 });
-console.log(UI);
