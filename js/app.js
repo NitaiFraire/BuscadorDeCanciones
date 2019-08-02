75eb350dec2f64ea919d4f3ab297ffbe35a0a57f
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-08-02 11:13:39 -0500

    enviando peticion y recibiendo la respuesta de api rest

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 import * as UI from './interfaz.js';
+import { API } from './api.js';
 
+UI.formularioBuscar.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    // obtener datos del fomulario
+    const artista = document.querySelector('#artista').value,
+          cancion = document.querySelector('#cancion').value;
+
+    // campos vacios
+    if(artista === '' || cancion === ''){
+
+        UI.divMensajes.innerHTML = 'Error... todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+        
+        setTimeout(() => {
+            
+            UI.divMensajes.innerHTML = '';
+            UI.divMensajes.classList.remove('error');
+
+
+        }, 3000);
+    
+    // formulario completo
+    }else{
+
+        const api = new API(artista, cancion);
+        api.consultarApi()
+           .then(data => {
+
+                console.log(data);
+           });
+    }
+
+});
 console.log(UI);
